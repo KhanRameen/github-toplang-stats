278be12b8614d4f9389e941e3124c2d48bc612d8
Rameen Khan <rameenk181@gmail.com>
2026-03-09 23:19:32 +0500

    core: update stats service response

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { StatsController } from './stats.controller';
 import { StatsService } from './stats.service';
 import { GithubService } from './github/github.service';
+import { SvgService } from './svg/svg.service';
 
 @Module({
   controllers: [StatsController],
-  providers: [StatsService, GithubService]
+  providers: [StatsService, GithubService, SvgService]
 })
 export class StatsModule {}
